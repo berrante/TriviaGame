92ba805944cfcf0e2114334af24ab9df490793d1
Bridget Errante <brierrante@gmail.com>
2017-05-11 14:14:18 -0400

    first upload

--- assets/javascript/app2.js
+++ assets/javascript/app2.js
@@ -41,41 +41,48 @@ var triviaQuestions = [{
 },
 ];
 
-var userChoice;
-/* on load */
-resetGame();
+//vars that stores user guesses
+var userGuess = [];
+var correctAnswers = 0;
+var incorrectAnswers = 0;
+var noAnswers = 0;
 
-//game begins with screen displaying "start" button only
-	function resetGame (){
-		$("button").text("START");
-		$(".quiz-container").hide();
-	}
+//make countdown timer
 
-//when user presses start, the following is displayed:
-//a list of questions with multiple choice buttons underneath them
-	$("button").on("click", function newGame (){
-		//when start is clicked
-		//remove start button
-		$("button").hide();
+
+//game starts with a screen displayig a start button
+window.onload = function() {
+	$('.quiz-container').hide();
+	newGame();
+}
+
+function newGame (){
+	//user presses start
+	$('button').on('click', function (){
+		//hide start button
+		$('.startButton').hide();
 		//display quiz
-		$(".quiz-container").show();
-		// display timer
-		var count=10;
+		$('.quiz-container').show();
+		//display countdown
+		$('.display').append("timer goes here");
+			//populate the prompt div with questions
+			for (i=0;i < triviaQuestions.length; i++){
+				$('.prompt').append(triviaQuestions[i].question + "<br>");
+					console.log(triviaQuestions[i].question);
+			}
+			//populate the choices div with answerList
+			for (i = 0; i < triviaQuestions.length; i++) {
+				$('.choices').append(triviaQuestions[i].answerList + '<input type="radio" name="radio_group">');
+			}	
+		})
+}
 
-		var counter=setInterval(timer, 1000); //1000 will  run it every 1 second
+/* GAME LOGIC */
 
-		function timer()
-		{
-		count=count-1;
-		if (count <= 0)
-		{
-		clearInterval(counter);
-		//counter ended, do something here
-		return;
-  		}
-  		//display timer here
-		$(".display").html("00:" + count);
-		//display question
-		$(".prompt0").html(triviaQuestions[0].question);
-		$(".prompt1").text(triviaQuestions[1].question);
-	}})
+//when time runs out, display a results page
+
+	//if the userGuess matches the correctAnswer, increase correctAnswers by 1
+		// if it doesnt match the correctAnswer, increase incorrectAnswers by 1
+			// if there was no answer, increase noAnswers by 1
+	
+	//display play again? button
